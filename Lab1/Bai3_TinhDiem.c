139a80ef8ebb1361b3fd4acef458b520447ff8eb
BaoHuynh1112 <Dfoulou2008@gmail.com>
2026-09-19 10:26:54 +0700

    <Hoan Thanh Lab1 Bai3>

---
+++ Lab1/Bai3_TinhDiem.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main (){
+    char masosinhvien[]= "PS52562";
+    char hovaten[]= "Huynh Nguyen Gia Bao";
+    float diemtoan =9.0;
+    float diemly= 8.5;
+    float diemhoa=9.5;
+    float diemTB=(diemtoan*2+diemly+diemhoa)/4;
+    printf ("MaSoSinhVien: %s\n",masosinhvien);
+    printf("HoVaTen: %s \n",hovaten);
+    printf ("DiemTB: %.f",diemTB);
+    return 0;
+}
