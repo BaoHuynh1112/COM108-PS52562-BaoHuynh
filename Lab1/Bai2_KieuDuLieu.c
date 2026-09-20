84935c69d2f69899f4b885063f3fd888de4a8d57
BaoHuynh1112 <Dfoulou2008@gmail.com>
2026-09-20 17:06:28 +0700

    <Hoan Thanh Lab1 Bai2>

--- Lab1/Bai2_KieuDuLieu.c
+++ Lab1/Bai2_KieuDuLieu.c
@@ -1,15 +1,16 @@
+//mssv,hoten,namsinh,tuoi,diemtb
 #include <stdio.h>
 int main (){
-    int namSinh;
-    float diemTB;
+    char mssv[]="PS52562";
+    char hoten[]="Huynh Nguyen Gia Bao";
+    int namSinh=2000;
+    int tuoi=2026-namSinh;
+    float diemTB=9.5;
 
-    printf("Nhap nam sinh: ");
-    scanf("%d",&namSinh);
-    printf("Nhap diem : ");
-    scanf("%f",&diemTB);
-    // xuat
-    printf("Nam Sinh: %d\n",namSinh);
-    printf("Tuoi: %d\n",2026- namSinh);
-    printf ("Diem TB: %f\n",diemTB);
+    printf("Ma So Sinh Vien:%s\n",mssv);
+    printf (" Ho va Ten: %s\n",hoten);
+    printf ("Nam Sinh: %d\n",namSinh);
+    printf ("Tuoi :%d\n",tuoi);
+    printf ("DIEMTB :%.2f\n",diemTB);
     return 0;
 }
