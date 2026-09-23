6e2b0e19f067fb9e3751e35f1edea25ed432aafa
BaoHuynh1112 <Dfoulou2008@gmail.com>
2026-09-23 21:54:04 +0700

    <Hoan Thanh Bai2 Lab2>

--- Lab2/Bai2_TinhDiem.c
+++ Lab2/Bai2_TinhDiem.c
@@ -1,14 +1,16 @@
+/*tinh diem tb cua 3 mon toan ly hoa
+diemtb=toan*3+ly*2+hoa/6*/
 #include <stdio.h>
 int main (){
     float diemToan,diemLy,diemHoa;
 
-    printf("\nNhap diem toan");
-    scanf("%f",&diemToan);
-    printf("\n Nhap diem Ly");
-    scanf("%f",&diemLy);
-    printf ("\n Nhap diem Hoa");
-    scanf("%f",&diemHoa);
+    printf ("\n Nhap Diem Toan:");
+    scanf ("%f",&diemToan);
+    printf ("\n Nhap Diem Ly:");
+    scanf ("%f",&diemLy);
+    printf("\n Nhap Diem Hoa:");
+    scanf ("%f",&diemHoa);
 
-    printf ("\nDiem TB :%.2f ",(diemToan*3+diemLy*2+diemHoa)/6);
+    printf ("\n Diem Trung Binh: %.2f",(diemToan*3+diemLy*2+diemHoa)/6);
     return 0;
 }
