663a20f4fd6eb4c9fc1cf2066b7844486a6576ea
BaoHuynh1112 <Dfoulou2008@gmail.com>
2026-09-23 21:31:36 +0700

    <Hoan Thanh Bai1 Lab2>

--- Lab2/Bai1_HinhHoc.c
+++ Lab2/Bai1_HinhHoc.c
@@ -1,26 +1,24 @@
-/* nhap xuat thong so chu vi dien tich hcn
-nhap xuat thong so chu vi dien tich ht #define PI 3.14159*/
+/* tinh chu vi va dien tich cua HCN va Hinh tron
+#define PI 3.14159*/
 #include <stdio.h>
 #define PI 3.14159
-
 int main (){
     float CDhcn,CRhcn;
     float bankinh;
 
-    printf ("\n Nhap Chieu Dai HCN: ");
-    scanf ("%f",&CDhcn);
+    printf("\n Nhap Chieu Dai HCN:");
+    scanf("%f",&CDhcn);
 
-    printf ("\n Nhap Chieu Rong HCN: ");
-    scanf("%f",&CRhcn);
+    printf("\n Nhap Chieu Rong HCN:");
+    scanf ("%f",&CRhcn);
 
-    printf ("\nChu Vi cua HCN la :%.2f",(CDhcn+CRhcn)*2);
-    printf ("\n Dien Tich cua HCN :%.2f",CDhcn*CRhcn);
+    printf ("\nChu vi cua HCN:%.2f",(CDhcn+CRhcn)*2);
+    printf("\nDien tich cua HCN:%.2f",CDhcn*CRhcn);
 
-    printf ("\n Nhap Ban Kinh :");
+    printf ("\n Nhap Ban Kinh Hinh Tron:");
     scanf("%f",&bankinh);
 
-    printf ("\nChu Vi cua Hinh Tron: %.2f",(bankinh+bankinh)*PI);
-    printf ("\nDien Tich cua Hinh Tron: %.2f",bankinh*2*PI);
-
+    printf ("\nChu vi cua hinh tron :%.2f",(bankinh+bankinh)*PI);
+    printf ("\nDien tich cua hinh tron :%.2f",bankinh*bankinh*PI);
     return 0;
 }
